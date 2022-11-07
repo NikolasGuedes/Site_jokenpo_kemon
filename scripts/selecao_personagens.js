32ed3e54087e2b595fcd827b13c2f9cc49905abe
Nikolas Guedes da Silva <guedesnikolas@gmail.com>
2022-11-07 20:34:29 -0300

    Versao Responsiva - ver 1.00

--- scripts/selecao_personagens.js
+++ scripts/selecao_personagens.js
@@ -10,31 +10,59 @@ const modo_batalha = document.getElementById('modo_batalha')
 const fundo_bloqueia_tela = document.getElementById('bloqueia_tela')
 
 const imagem_pokemon = document.getElementById('imagem-pokemon')
+const imagem_pokemon_celular = document.getElementById('imagem-pokemon-celular')
+
 const nome_pokemon = document.getElementById('nome-pokemon')
+
 const efeito_bolha_bulbassauro = document.getElementById('bolha-bulbassauro')
 const efeito_bolha_squirlte = document.getElementById('bolha-squirtle')
 const efeito_bolha_charmander = document.getElementById('bolha-charmander')
+
 const fundo_batalha = document.getElementById('fundo-batalha')
+const fundo_batalha_celular = document.getElementById('fundo-batalha-celular')
 
 const fundo_contador_vitorias = document.querySelectorAll('.texto__contador--vitorias')
+const fundo_contador_vitorias_celular = document.querySelectorAll('.texto__contador--vitorias-celular')
 
 
 cartaBulbassauro.forEach((elemento) => {
 
     elemento.addEventListener("click", chamaTransicaoBatalha)
 
-   
+    elemento.addEventListener("mouseover", () => {
+
+        corFundo.style.background = "#33CC33";
+        fundoAnimado.classList.add("fundo_animado_bulbassauro")
+
+    })
+
+    elemento.addEventListener("mouseout", () => {
+
+        corFundo.style.background = "#FFCC96";
+        fundoAnimado.classList.remove("fundo_animado_bulbassauro")
+
+    })
 
     elemento.addEventListener("click", () => {
 
         console.log("Player escolheu Bulbassauro")
 
         imagem_pokemon.classList.add('personagem_bulbassauro')
+        imagem_pokemon_celular.classList.add('personagem_bulbassauro')
+
         nome_pokemon.textContent = "Bulbassauro"
+
         efeito_bolha_bulbassauro.classList.add('ativa')
+
         fundo_batalha.classList.add('fundo_bulbassauro')
+        fundo_batalha_celular.classList.add('fundo_bulbassauro')
 
         fundo_contador_vitorias.forEach((elemento) => {
+
+            elemento.classList.add("contador_bulbassauro")
+        })
+
+        fundo_contador_vitorias_celular.forEach((elemento) => {
 
             elemento.classList.add("contador_bulbassauro")
         })
@@ -47,18 +75,40 @@ cartaSquirtle.forEach((elemento) => {
 
     elemento.addEventListener("click", chamaTransicaoBatalha)
 
-   
+    elemento.addEventListener("mouseover", () => {
+
+        fundoAnimado.classList.add("fundo_animado_squirtle")
+        corFundo.style.background = "#7DDFFF";
+
+    })
+
+    elemento.addEventListener("mouseout", () => {
+
+        fundoAnimado.classList.remove("fundo_animado_squirtle")
+        corFundo.style.background = "#FFCC96";
+
+    })
 
     elemento.addEventListener("click", () => {
 
         console.log("Player escolheu Squirtle")
 
         imagem_pokemon.classList.add('personagem_squirtle')
+        imagem_pokemon_celular.classList.add('personagem_squirtle')
+
         nome_pokemon.textContent = "squirtle"
+
         efeito_bolha_squirlte.classList.add('ativa')
+
         fundo_batalha.classList.add('fundo_squirtle')
+        fundo_batalha_celular.classList.add('fundo_squirtle')
 
         fundo_contador_vitorias.forEach((elemento) => {
+
+            elemento.classList.add("contador_squirtle")
+        })
+
+        fundo_contador_vitorias_celular.forEach((elemento) => {
 
             elemento.classList.add("contador_squirtle")
         })
@@ -71,23 +121,48 @@ cartacharmander.forEach((elemento) => {
 
     elemento.addEventListener("click", chamaTransicaoBatalha)
 
-  
+    elemento.addEventListener("mouseover", () => {
+
+        fundoAnimado.classList.add("fundo_animado_charmander")
+        corFundo.style.background = "#FF8700";
+
+    })
+
+    elemento.addEventListener("mouseout", () => {
+
+        fundoAnimado.classList.remove("fundo_animado_charmander")
+        corFundo.style.background = "#FFCC96";
+
+    })
 
     elemento.addEventListener("click", () => {
+
 
         console.log("Player escolheu Charmander")
 
         imagem_pokemon.classList.add('personagem_charmander')
-        nome_pokemon.textContent = "charmander"
-        efeito_bolha_charmander.classList.add('ativa')
-        fundo_batalha.classList.add('fundo_charmander')
+        imagem_pokemon_celular.classList.add('personagem_charmander')
 
+        nome_pokemon.textContent = "charmander"
+
+        efeito_bolha_charmander.classList.add('ativa')
+
+        fundo_batalha.classList.add('fundo_charmander')
+        fundo_batalha_celular.classList.add('fundo_charmander')
+    
         fundo_contador_vitorias.forEach((elemento) => {
+    
+            elemento.classList.add("contador_charmander")
+        })
+
+        fundo_contador_vitorias_celular.forEach((elemento) => {
 
             elemento.classList.add("contador_charmander")
         })
 
     })
+
+    
 
 })
 
